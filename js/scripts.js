ce1bda045a279e12db22eca95fb08d484e8b1310
vjmurenko <vjmurenko@gmail.com>
2024-12-28 02:29:45 +0300

    GoodVersion

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ let ShowNavBar = false;
 let nav = document.getElementById("NavBar");
 let hidNavBarButton = document.getElementById("HiddenNavBarButton");
 let showNavBarButton = document.getElementById("ShowNavBarButton");
+const ENGLISH = "EN";
+const RUSSIAN = "RU";
+const RuHref = "BookRu.html";
+const EnHref = "BookEn.html";
+
 
 hidNavBarButton.addEventListener("click", () => {
     nav.classList.add("d-none");
@@ -50,3 +55,31 @@ backgroundSelector.addEventListener('change', function() {
     // Add the selected background class
     contentContainer.classList.add(this.value);
 });
+
+function setSelectedLanguage() {
+    const languageSelect = document.getElementById('languageSelect');
+    const currentPage = window.location.pathname.split('/').pop(); 
+
+    if (currentPage === EnHref) {
+        languageSelect.value = ENGLISH;
+    } else if (currentPage === RuHref) {
+        languageSelect.value = RUSSIAN;
+    }
+}
+
+// Function to handle language change
+function changeLanguage() {
+    const selectedLanguage = document.getElementById('languageSelect').value;
+    if (selectedLanguage === ENGLISH) {
+        window.location.href = EnHref; 
+    } else if (selectedLanguage === RUSSIAN) {
+        window.location.href = RuHref; 
+    }
+}
+
+window.onload = setSelectedLanguage;
+
+document.getElementById('languageSelect').addEventListener('change', changeLanguage);
+
+
+
